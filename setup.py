0fb134de9859c745c4d4a92329d44cae1784a140
Phil <91705643+philstrz@users.noreply.github.com>
2022-07-04 12:26:22 -0700

    Added requirements
    Let's see if it works?

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='v2',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='phils',
+    author_email='',
+    description=''
+)
